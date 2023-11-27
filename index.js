a3edda873d98984536fb51caa324d1c45beb6f2f
mahi239 <148883188+mahi239@users.noreply.github.com>
2023-11-27 16:29:15 +0000

    commit added

---
+++ index.js
@@ -0,0 +1,6 @@
+const planets = ["Earth", "Saturn", "Mercury", "Venus", "Neptune"];
+
+const satellite = ["Sputnik", "Arayabhatta", "G-SAT"];
+const galaxy = planets.concat(satellite);
+const newGalaxy = galaxy.toLowerCase();
+console.log(newGalaxy);
